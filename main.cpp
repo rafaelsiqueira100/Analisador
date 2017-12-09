76ecae002300bb6380364c945f06321837eee842
Matheus-Fernandes <matheusfsilva10@gmail.com>
2017-12-09 19:37:03 -0200

    nova main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,31 @@
 
 int main()
 {
-    AnalisadorSintatico a("teste3.txt");
-    a.CompInicioPrograma();
+    string arquivos[] = {
+        "Certo.pas",
+        "ConflitoDeTipo1.pas",
+        "ConflitoDeTipo2.pas",
+        "ConflitoDeTipo3.pas",
+        "FaltaDeDo.pas",
+        "FaltaDePontoFinal.pas",
+        "FaltaDeThen.pas",
+        "FaltaPontoEVigula1.pas",
+        "FaltaPontoEVigula2.pas",
+        "FaltaPontoEVigula3.pas",
+        "IdentificadorComecandoComDigito.pas",
+        "IdentificadorNaoDeclarado.pas",
+        "ParentesesDesvalanceados.pas",
+        "PontoEVirgulaAntesDeElse.pas"
+    };
+
+    for (int i = 0; i < 14; i++){
+        cout << "Nome do Arquivo : " << arquivos[i] << endl ;
+
+        AnalisadorSintatico a("arquivos/" + arquivos[i]);
+        a.CompInicioPrograma();
+
+        cout << endl << endl ;
+    }
 
     return 0;
 }
