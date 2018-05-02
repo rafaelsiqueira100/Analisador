366af29b335d61f8d57fc257bd868dce2516fb24
rafaelsiqueira100 <rafaelsiqueira100@gmail.com>
2018-05-02 09:21:10 -0300

    Todos os arquivos errados atualizados e criados

---
+++ arquivos/39-Parenteses6.pas
@@ -0,0 +1,49 @@
+Program Prog1;
+    Var a, b: Integer;
+
+    Procedure Proc1 (Boolean b);
+        Var a, c: Boolean;
+
+        Function Func1 (Integer a): Integer;
+            Var b, d: Integer;
+        Begin
+            b := 2*a-1;
+
+            While (b>0) Do
+            Begin
+                d := b;
+                b := 2*d-1;
+            End
+
+            Func1 := (b+d)/2;
+        End
+    Begin
+        If (Func1(5)/2 >7)
+        Then Begin
+            a := true;
+            c := not b;
+        End
+        Else Begin
+            a := not b;
+            c := 2+3*5/7 < (8 or c = c;
+        End
+
+        If (a and b or c) Then
+            Write ('COTUCA');
+        Else
+            Write ('UNICAMP');
+    End
+Begin
+    a := 5;
+    b := 7;
+
+    While (a<b or (a>500 and b>700)) Do
+    Begin
+        Proc1(a<b);
+        a := a+2;
+        b := 2*b;
+    End
+
+    Write (a);
+    Write (b);
+End.
